513c8d66f7b23badd4f5d6fdd0c50f8048045524
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-03-28 08:07:27 +0100

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 const Card = ( { character } ) => {
   return (
-    <div className="bg-black text-white h-80 rounded-lg shadow-md">
-      <img className="w-32 h-32 rounded-full mx-auto mt-7" src={character.image} alt="character" />
+    <div className="bg-black border-solid text-white h-80 rounded-lg shadow-md">
+      <img className="w-32 h-32 rounded mx-auto mt-7" src={character.image} className="card-img-top" alt="character" />
       <div className="text-center mt-5">
         <h5 className="mb-0">{character.name}</h5>
         <p>
