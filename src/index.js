e5a866d7f0b944fbe76a212252b3500118097324
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-03-28 08:09:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import App from './App'
+import './index.css';
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
