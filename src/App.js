e3e259152bebca57c5ddcdab570e062409074b3b
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2022-04-03 15:16:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useQuery } from '@apollo/client'
 import { GET_ALL_CHARACTERS } from './queries'
 import Card from './Card'
 
+import {useEffect, useState} from 'react'
+
 const App = () => {
   const { loading, error, data } = useQuery(GET_ALL_CHARACTERS, {
     variables: { page: 1 }
@@ -9,14 +11,46 @@ const App = () => {
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error {error.message}</p>
+  
+  const [characters, setCharacters] = useState()
+  const [filterQuery, setFilterQuery] = useState()
+  
+  useEffect(() => {
+    if (!filterQuery) {
+      setCharacters(data?.characters?.results)
+    } else {
+      const queryString = filterQuery.toLowerCase()
+      const filteredData = data?.characters?.results?.filter(character => {
+        const name = `${character.name}`
+
+        // if it's just one letter, return all names that start with it
+        if (queryString.length === 1) {
+          const firstLetter = name.charAt(0).toLowerCase()
+          return firstLetter === queryString
+        }
+        else {
+          return name.toLowerCase().includes(queryString)
+        }
+      })
+      setCharacters(filteredData)
+    }
+  }, [data, filterQuery])
 
   return (
     <>
       <header>
         <h1 className={"text-center m-3"}>Rick and Morty GraphQL App</h1>
       </header>
+      <form>
+          <input
+            type={"text"}
+            placeholder={"type here to filter..."}
+            onChange={event => setFilterQuery(event.target.value)}
+            className={"ml-20 mt-6 rounded-md p-2"}
+          />
+        </form>
       <div className={"bg-black m-auto grid md:grid-cols-3 gap-6 p-20"}>
-        {data?.characters?.results.map(character =>
+        {characters.map(character =>
           <Card character={character} key={character.id} />
         )}
       </div>
